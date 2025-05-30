03ab9ab3a0b8f64b3235836d552b36cd0a431eca
Hayden <z5592258@ad.unsw.edu.au>
2025-05-30 15:43:44 +1000

    feat: added datastore

---
+++ src/dataStore.ts
@@ -0,0 +1,16 @@
+// DATA FOR USERS AND QUIZZES
+let data = {
+    users:[],
+    quizzes:[]
+};
+
+
+// Use getData() to access the data
+function getData() {
+    return data;
+}
+  
+export { getData };
+
+  
+  
