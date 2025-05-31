32725f53327b93b0464d49c60e45cd8acf8e6e2d
Hayden <z5592258@ad.unsw.edu.au>
2025-05-31 20:55:27 +1000

    test: wrote tests for adminUserPaswordUpdate

--- src/auth_tests/adminUserPasswordUpdate.test.ts
+++ src/auth_tests/adminUserPasswordUpdate.test.ts
@@ -1,5 +1,67 @@
-describe.skip('Success cases', () => {
-    test('Successfully...', () => {
+import { adminUserPasswordUpdate, adminAuthRegister } from '../auth';
+import { clear } from '../other';
 
+beforeEach(() => {
+    clear();
+});
+
+describe('adminUserPasswordUpdate', () => {
+    let userIdValid: number;
+    
+    beforeEach(() => {
+        userIdValid = adminAuthRegister('valid@mail.com', 'ValidPassword1', 'validFirst', 'validLast').userId;
     });
-})
+
+    describe('Error Cases', () => {
+        test('Invalid UserId', () => {
+            let userIdInvalid = userIdValid + 1;
+            expect(adminUserPasswordUpdate(userIdInvalid, 'ValidPassword1', 'ValidPassword2')).toStrictEqual({ error: expect.any(String) });
+        });
+
+        test('Old Password is not the correct old password', () => {
+            expect(adminUserPasswordUpdate(userIdValid, 'InvalidPassword1', 'ValidPassword2')).toStrictEqual({ error: expect.any(String) });
+        });
+
+        test('Old Password and New Password match exactly', () => {
+            expect(adminUserPasswordUpdate(userIdValid, 'ValidPassword1', 'ValidPassword1')).toStrictEqual({ error: expect.any(String) });
+        });
+
+        test('New Password has already been used before by this user', () => {
+            // Successfully changes password to Val!dPassword2
+            expect(adminUserPasswordUpdate(userIdValid, 'ValidPassword1', 'ValidPassword2')).toStrictEqual({});
+            // Successfully changes password to Val!dPassword3
+            expect(adminUserPasswordUpdate(userIdValid, 'ValidPassword2', 'ValidPassword3')).toStrictEqual({});
+            // Should fail to change password back to Val!dPassword2
+            expect(adminUserPasswordUpdate(userIdValid, 'ValidPassword3', 'ValidPassword2')).toStrictEqual({ error: expect.any(String) });
+            // Should also fail to change password to Val!dPassword1
+            expect(adminUserPasswordUpdate(userIdValid, 'ValidPassword3', 'ValidPassword1')).toStrictEqual({ error: expect.any(String) });
+        });
+
+        test.each([
+            { test: 'at least one number', oldPassword: 'ValidPassword1', newPassword: 'invalidPwd' },
+            { test: 'at least one letter', oldPassword: 'ValidPassword1', newPassword: '123456789' },
+            { test: 'at least 8 characters', oldPassword: 'ValidPassword1', newPassword: 'ValidP1' }
+        ])('New Password does not have: $test', ({ oldPassword, newPassword }) => {
+            expect(adminUserPasswordUpdate(userIdValid, oldPassword, newPassword)).toStrictEqual({ error: expect.any(String) });
+        });
+
+        test.each([
+            { oldPassword: 'ValidPassword1', newPassword: 'qwerty123' },
+            { oldPassword: 'ValidPassword1', newPassword: 'password1' },
+            { oldPassword: 'ValidPassword1', newPassword: '1q2w3e4r5t' },
+            { oldPassword: 'ValidPassword1', newPassword: '1q2w3e4r' },
+        ])('New Password is valid but is too common(according to Nordpass, NSCS and Keeper)', ({ oldPassword, newPassword }) => {
+            expect(adminUserPasswordUpdate(userIdValid, oldPassword, newPassword)).toStrictEqual({ error: expect.any(String) });
+        });
+    });
+
+    describe('Success Cases', () => {
+        test.each([
+            { test: 'mainly letters', oldPassword: 'ValidPassword1', newPassword: 'ValidPassword2' },
+            { test: 'mainly numbers', oldPassword: 'ValidPassword1', newPassword: '12345678P' },
+            { test: 'special characters', oldPassword: 'ValidPassword1', newPassword: 'ValidPassword!' }
+        ])('Password successfully updated: $test', ({ oldPassword, newPassword }) => {
+            expect(adminUserPasswordUpdate(userIdValid, oldPassword, newPassword)).toStrictEqual({});
+        });
+    });
+});
