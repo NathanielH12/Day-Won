234c4c8a7b057bc8d7853c2c90244e81f9e04f03
Hayden <z5592258@ad.unsw.edu.au>
2026-02-08 18:26:06 +1100

    docs: converted main.js to typescript

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import {
+    startTimer,
+    resetTimer,
+    customiseTimer
+} from "./timer";
+
+const timer = document.getElementById('timer');
+const startBtn = document.getElementById('startBtn');
+const resetBtn = document.getElementById('resetBtn');
+const customiseBtn = document.getElementById('customiseBtn');
+
+startBtn.addEventListener('click', () => startTimer(startBtn, timer));
+resetBtn.addEventListener('click', () => resetTimer(startBtn, timer));
+customiseBtn.addEventListener('click', () => customiseTimer(startBtn, timer));
