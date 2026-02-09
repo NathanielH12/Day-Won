ad2ec631e28913b15af7695c55a86d3caf2ca31a
Nathaniel <Nathaniel@student.unsw.edu.au>
2026-02-09 22:18:32 +1100

    feat: add increment and decrement buttons functionality for customising timer

--- src/main.js
+++ src/main.js
@@ -8,6 +8,16 @@ const timer = document.getElementById('timer');
 const startBtn = document.getElementById('startBtn');
 const resetBtn = document.getElementById('resetBtn');
 const customiseBtn = document.getElementById('customiseBtn');
+const hourUpBtn = document.getElementById('hourUpBtn');
+const hourDownBtn = document.getElementById('hourDownBtn');
+
+hourUpBtn.style.display = 'none';
+hourDownBtn.style.display = 'none';
+
+function hideCustomiseBtns() {
+    hourUpBtn.style.display = 'none';
+    hourDownBtn.style.display = 'none';
+}
 
 function startTimer() {
     if (isTimerOn === true) {
@@ -21,6 +31,8 @@ function startTimer() {
         isTimerOn = true;
         startBtn.innerHTML = 'Pause';
     }
+
+    hideCustomiseBtns();
 }
 
 function updateDisplay() {
@@ -56,13 +68,36 @@ function resetTimer() {
     startBtn.innerHTML = 'Start';
 
     updateDisplay();
+    hideCustomiseBtns();
+}
+
+function incrementHour() {
+    // set max hours restriction?
+    time += 60 * 60 * 100;
+    updateDisplay();
+}
+
+function decrementHour() {
+    const decrementedTime = time - (60 * 60 * 100);
+    if (decrementedTime <= 0) {
+        // do nothing
+    } else {
+        time = decrementedTime;
+        updateDisplay();
+    }
 }
 
 function customiseTimer() {
     resetTimer();
     // then begin adding functionality for customising the timer
+
+    // when pressed increment, decrement and save buttons pop up
+    hourUpBtn.style.display = 'block';
+    hourDownBtn.style.display = 'block';
 }
 
 startBtn.addEventListener('click', startTimer);
 resetBtn.addEventListener('click', resetTimer);
 customiseBtn.addEventListener('click', customiseTimer);
+hourUpBtn.addEventListener('click', incrementHour);
+hourDownBtn.addEventListener('click', decrementHour);
